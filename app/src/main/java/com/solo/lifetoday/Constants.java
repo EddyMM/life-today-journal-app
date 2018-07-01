259b7373e3e80ef3c80783efb853175a55944911
eddy <mwendaeddy@gmail.com>
2018-07-01 18:36:30 +0300

    Ensure each user has their on Firebase journal

--- app/src/main/java/com/solo/lifetoday/Constants.java
+++ app/src/main/java/com/solo/lifetoday/Constants.java
@@ -1,10 +1,13 @@
 package com.solo.lifetoday;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 /**
  * @author eddy.
  */
 
 public class Constants {
     public static final int ENTRY_ITEM_EXCERPT_SIZE = 60;
-    public static final String JOURNAL_DB_REF = "journal";
+    public static final String JOURNAL_DB_REF =
+            "/users/" + FirebaseAuth.getInstance().getCurrentUser().getUid() + "/journal";
 }
